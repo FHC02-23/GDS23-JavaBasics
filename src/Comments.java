cf42151f39f6ae83162760d87e6b0293723ce5e1
Daniel Zettel <daniel.zettel@lv.campus02.at>
2023-09-29 20:17:10 +0200

    comments demo

---
+++ src/Comments.java
@@ -0,0 +1,20 @@
+public class Comments {
+
+
+    // bin ein Kommentar, nur eine zeile
+
+
+    /**
+     * javadoc Kommentar
+     * @param args Parameter der Methode
+     */
+    public static void main(String[] args) {
+        /*
+        Blockkommentar
+        geht über mehrere Zeilen, wenn gewünscht
+
+
+
+         */
+    }
+}
