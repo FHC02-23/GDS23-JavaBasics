22c4c071348f80baa23d4b5728c94948a9733b59
Daniel Zettel <daniel.zettel@lv.campus02.at>
2023-09-29 19:12:43 +0200

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,13 @@
+public class MyFirstClass {
+
+    // psvm + tab
+    // main + tab
+    public static void main(String[] args) {
+        // erste Ausgabe
+        // sout + tab
+        System.out.println("Ich werde in der Konsole angezeigt");
+
+
+        System.out.println("Programm Ende");
+    }
+}
