b5b947b7877fe304258f50d4f1c5c32abe89fa81
Daniel Zettel <daniel.zettel@lv.campus02.at>
2023-10-06 19:18:16 +0200

    methods for calculations

--- src/Calculator.java
+++ src/Calculator.java
@@ -18,9 +18,62 @@ public class Calculator {
         // STRG + ALT + V => variable generiert
         int result1 = a + b;
 
-        System.out.println("Mein Name ist: " + name);
-        System.out.println("a: " + a); // STRG + D
-        System.out.println("b: " + b); // STRG + D
-        System.out.println("result = " + result); // soutv + tab
+//        System.out.println("Mein Name ist: " + name);
+//        System.out.println("a: " + a); // STRG + D
+//        System.out.println("b: " + b); // STRG + D
+//        System.out.println("result = " + result); // soutv + tab
+
+        int res = add(1, 2);
+        printResult(res);
+
+        res = subtract(3, 2);
+        printResult(res);
+
+        printResult(multiply(3, 2));
+
+        printResult(divide(3,2));
+        printResult(mod(3, 2));
+
+    }
+
+    public static int add(int input1, int input2) {
+        System.out.println("Input 1: " + input1);
+        System.out.println("Input 2: " + input2);
+        int result = input1 + input2;
+
+        //System.out.println("Ergebnis: " + result);
+        System.out.println("Ergebnis: " + (input1 + input2));
+        return result;
+
+        // 2. Möglichkeit
+        // direkt das Ergebnis retour liefern
+        //return input1 + input2;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public static double divide(int a, double b) {
+        // cast to double
+        // 1. Möglichkeit --> beide input parameter sind int
+        //double result = a / (b * 1.0);
+        // return result
+
+        // 2. Möglichkeit
+        // Parameter zu double ändern
+        return a / b;
+    }
+
+    public static int mod(int a, int b) {
+        return a % b;
+    }
+
+    public static void printResult(double result) {
+        System.out.println("Das Ergebnis lautet: " + result);
     }
 }
